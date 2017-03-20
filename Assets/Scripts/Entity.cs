01f39587c49884e0879de5d0a53e92426ee73d2a
Teun van Laarhoven <teunvanlaarhoven@gmail.com>
2017-03-20 12:39:54 +0100

    added empty classes

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public float X;
+    public float Y;
+    public float Xvel;
+    public float Yvel;
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    public float RoF;
+    public int ShieldPower;
+    public float Direction;
+    public PowerUp PowerUp;
+
+    private Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    mousePosition = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePosition = Input.mousePosition;
+    }
+}
